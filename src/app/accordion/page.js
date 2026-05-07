39f0c23e37ce8be28ea4b1dcf651928985881300
Tejas <mtsn9699@gmail.com>
2026-05-07 22:27:48 +0530

    Accordion ADU

--- src/app/accordion/page.js
+++ src/app/accordion/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import AccordionBox from "@/components/AccordionBox";
+import { Plus } from "lucide-react";
 
 export default function Accordion() {
   const [accordion, setAccordion] = useState([
@@ -62,23 +63,138 @@ export default function Accordion() {
     },
   ]);
   const [isOpenId, setIsOpenId] = useState(null);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updatingAccordion, setUpdatingAccordion] = useState(null);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
   function toggleState(id) {
     if (isOpenId == null) setIsOpenId(id);
     else if (isOpenId != id) setIsOpenId(id);
     else setIsOpenId(null);
   }
+
+  function addAccordion() {
+    if (!question.trim()) return;
+    if (!answer.trim()) return;
+
+    const newAccordion = {
+      id: nanoid(),
+      question: question,
+      answer: answer,
+    };
+    setAccordion(() => {
+      return [...accordion, newAccordion];
+    });
+    setIsAdding(false);
+    setQuestion("");
+    setAnswer("");
+  }
+
+  function deleteAccordion(id) {
+    const newArr = accordion.filter((item) => id != item.id);
+    setAccordion(newArr);
+  }
+
+  function updateAccordion() {
+    if (!question.trim()) return;
+    if (!answer.trim()) return;
+
+    const updatedAccordion = accordion.map((item) => {
+      if (updatingAccordion.id == item.id) {
+        return { ...item, question: question, answer: answer };
+      }
+
+      return item;
+    });
+
+    setAccordion(updatedAccordion);
+    setQuestion("");
+    setAnswer("");
+    setIsUpdating(false);
+  }
   return (
     <>
-      <h1 className="text-6xl m-4">Accordion</h1>
+      <div className="flex justify-around items-center">
+        <h1 className="text-6xl m-4">Accordion</h1>
+        <Plus
+          onClick={() => {
+            setIsAdding(true);
+          }}
+        />
+      </div>
       <div>
-        {accordion.map((item) => {
-          return (
-            <div key={item.id} className="">
-                <AccordionBox item={item} openId={isOpenId} toggleState={toggleState}/>
-            </div>
-          );
-        })}
+        {isAdding ? (
+          <div className="border rounded-xl p-4 shadow-md flex flex-col gap-3 bg-white">
+            <input
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              type="text"
+              placeholder="Add question"
+              className="border rounded-lg p-2 outline-none focus:ring-2 focus:ring-gray-400"
+            />
+
+            <input
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              type="text"
+              placeholder="Add answer"
+              className="border rounded-lg p-2 outline-none focus:ring-2 focus:ring-gray-400"
+            />
+
+            <button
+              onClick={isAdding ? addAccordion : updateAccordion}
+              className="bg-black text-white rounded-lg p-2 hover:bg-gray-800 transition-colors"
+            >
+              {isAdding ? "Add Accordion" : "Update Accordion"}
+            </button>
+          </div>
+        ) : isUpdating ? (
+          <div className="border rounded-xl p-4 shadow-md flex flex-col gap-3 bg-white">
+            <input
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              type="text"
+              placeholder="Add question"
+              className="border rounded-lg p-2 outline-none focus:ring-2 focus:ring-gray-400"
+            />
+
+            <input
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              type="text"
+              placeholder="Add answer"
+              className="border rounded-lg p-2 outline-none focus:ring-2 focus:ring-gray-400"
+            />
+
+            <button
+              onClick={isAdding ? addAccordion : updateAccordion}
+              className="bg-black text-white rounded-lg p-2 hover:bg-gray-800 transition-colors"
+            >
+              {isAdding ? "Add Accordion" : "Update Accordion"}
+            </button>
+          </div>
+        ) : (
+          <>
+            {accordion.map((item) => {
+              return (
+                <div key={item.id} className="">
+                  <AccordionBox
+                    item={item}
+                    openId={isOpenId}
+                    toggleState={toggleState}
+                    deleteAccordion={deleteAccordion}
+                    setIsUpdating={setIsUpdating}
+                    setUpdatingAccordion={setUpdatingAccordion}
+                    setQuestion={setQuestion}
+                    setAnswer={setAnswer}
+                  />
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </>
   );
--- src/components/AccordionBox.js
+++ src/components/AccordionBox.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Trash2, Pencil } from "lucide-react";
 
-const AccordionBox = ({ item, openId, toggleState }) => {
+const AccordionBox = ({ item, openId, toggleState , deleteAccordion, setIsUpdating, setUpdatingAccordion, setQuestion, setAnswer }) => {
   return (
     <>
-      <div className={`border rounded-xl p-4 mb-4 shadow-md  ${openId === item.id ? "bg-gray-400" : "bg-white hover:bg-gray-200"}`}>
+      <div
+        className={`border rounded-xl p-4 mb-4 shadow-md  ${openId === item.id ? "bg-gray-400" : "bg-white hover:bg-gray-200"}`}
+      >
         <button
           className="w-full flex justify-between items-center"
           onClick={() => {
@@ -11,8 +14,21 @@ const AccordionBox = ({ item, openId, toggleState }) => {
           }}
         >
           <span className="text-lg font-semibold">{item.question}</span>
-          <span className="text-2xl font-bold cursor-pointer">
+          <span className="flex items-center gap-2 text-2xl font-bold">
             {openId === item.id ? "-" : "+"}
+
+            <Trash2 className="w-5 h-5 cursor-pointer" onClick={(e)=>{
+                e.stopPropagation();
+                deleteAccordion(item.id)
+            }}/>
+
+            <Pencil className="w-5 h-5 cursor-pointer" onClick={(e)=>{
+                e.stopPropagation();
+                setIsUpdating(true)
+                setUpdatingAccordion(item)
+                setQuestion(item.question)
+                setAnswer(item.answer)
+            }}/>
           </span>
         </button>
 
