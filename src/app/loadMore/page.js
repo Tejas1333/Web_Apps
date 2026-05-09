36c3e83529efebf878942d0262c0c415104446b1
Tejas <mtsn9699@gmail.com>
2026-05-09 19:26:32 +0530

    Load More Data

--- src/app/loadMore/page.js
+++ src/app/loadMore/page.js
@@ -3,10 +3,57 @@ import React from "react";
 import { useState, useEffect } from "react";
 import MovieCard from "@/components/MovieCard";
 import { movies } from "@/data/movies";
+import { ImSpinner2 } from "react-icons/im";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function LoadMore() {
-  const [visibleData, setVisibleData] = useState(4);
+  const [visibleData, setVisibleData] = useState(8);
   const result = movies.slice(0, visibleData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 400) {
+        setShowButton(true);
+      } else setShowButton(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function loadFunction() {
+    if (visibleData >= movies.length) return;
+    setIsLoading(true);
+    setTimeout(() => {
+      setVisibleData((prev) => prev + 4);
+      setIsLoading(false);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    function handleInfiniteScroll() {
+      const bottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 200;
+
+      if (bottom && !isLoading && visibleData < movies.length) loadFunction();
+    }
+
+    window.addEventListener("scroll", handleInfiniteScroll);
+
+    return () => window.removeEventListener("scroll", handleInfiniteScroll);
+  }, [isLoading, visibleData]);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <>
       <div className="min-h-screen bg-gray-400 p-10">
@@ -18,9 +65,59 @@ export default function LoadMore() {
           ))}
         </div>
 
+        {isLoading && (
+          <div className="flex justify-center mt-10">
+            <ImSpinner2 className="animate-spin text-4xl text-white" />
+          </div>
+        )}
+
+        {showButton && (
+          <button
+            onClick={scrollToTop}
+            className="
+        fixed
+        bottom-8
+        right-8
+        z-50
+
+        flex
+        items-center
+        justify-center
+
+        w-14
+        h-14
+
+        rounded-full
+
+        bg-white/80
+        backdrop-blur-xl
+
+        border
+        border-white/40
+
+        shadow-[0_8px_30px_rgba(0,0,0,0.2)]
+
+        text-slate-800
+        text-xl
+
+        transition-all
+        duration-300
+
+        hover:scale-110
+        hover:bg-black
+        hover:text-white
+        hover:shadow-[0_12px_40px_rgba(0,0,0,0.35)]
+
+        active:scale-95
+      "
+          >
+            <FaArrowUp />
+          </button>
+        )}
+
         {/* Bottom Section */}
         <div className="flex justify-center mt-14">
-          {visibleData >= movies.length ? (
+          {visibleData >= movies.length && (
             <div
               className="
                 px-8
@@ -38,50 +135,6 @@ export default function LoadMore() {
             >
               🎬 No More Movies Left
             </div>
-          ) : (
-            <button
-              onClick={() => {
-                setVisibleData((prev) => prev + 4);
-              }}
-              className="
-                group
-                relative
-                overflow-hidden
-                px-10
-                py-4
-                rounded-2xl
-                bg-white
-                text-slate-800
-                font-bold
-                text-lg
-                shadow-[0_8px_25px_rgba(0,0,0,0.15)]
-                transition-all
-                duration-300
-                hover:scale-105
-                hover:shadow-[0_12px_35px_rgba(0,0,0,0.25)]
-                "
-                >
-              <span className="relative z-10">Load More Movies</span>
-
-              {/* Shine Animation */}
-              <div
-                className="
-                absolute
-                top-0
-                left-[-120%]
-                h-full
-                w-full
-                bg-linear-to-r
-                from-transparent
-                via-white/50
-                to-transparent
-                skew-x-12
-                transition-all
-                duration-700
-                group-hover:left-[120%]
-                "
-              />
-            </button>
           )}
         </div>
       </div>
