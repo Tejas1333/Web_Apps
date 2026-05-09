c5a3facb5e6d35ef4331eef55992ec9741e6af57
Tejas <mtsn9699@gmail.com>
2026-05-09 17:32:18 +0530

    ImageSlider

--- src/app/imageSlider/page.js
+++ src/app/imageSlider/page.js
@@ -3,7 +3,9 @@ import React from "react";
 import Image from "next/image";
 import ImageBox from "@/components/ImageBox";
 import { useState, useEffect } from "react";
-import { GrNext, GrPrevious, GoCircle } from "react-icons/gr";
+import { GrNext, GrPrevious } from "react-icons/gr";
+import { GoCircle, GoDotFill } from "react-icons/go";
+import { RxCross1 } from "react-icons/rx";
 
 export default function ImageSlider() {
   const images = [
@@ -13,16 +15,18 @@ export default function ImageSlider() {
     "/images/taro-sakamoto.png",
   ];
   const [index, setIndex] = useState(0);
+  const [isFull, setIsFull] = useState(false);
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    setIndex((prev) => {
-      return (prev + 1) % images.length;
-    });
-  }, 3000);
+  useEffect(() => {
+    if (isFull) return;
+    const interval = setInterval(() => {
+      setIndex((prev) => {
+        return (prev + 1) % images.length;
+      });
+    }, 3000);
 
-  return () => clearInterval(interval);
-}, []);
+    return () => clearInterval(interval);
+  }, [isFull]);
 
   function prevImage() {
     setIndex((prev) => {
@@ -36,33 +40,79 @@ useEffect(() => {
     });
   }
 
+  useEffect(() => {
+    function handleKeyboard(e) {
+      if (e.key === "ArrowRight") nextImage();
+      if (e.key === "ArrowLeft") prevImage();
+    }
+
+    window.addEventListener("keydown", handleKeyboard);
+
+    return () => window.removeEventListener("keydown", handleKeyboard);
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-100 via-purple-100 to-pink-100">
-      <div className="bg-white/40 backdrop-blur-md p-6 rounded-3xl shadow-xl border border-white/30">
-        <div className="overflow-hidden rounded-2xl ">
-          <ImageBox image={images[index]} />
+      {isFull ? (
+        <div className="fixed inset-0 bg-black/40 backdrop-blur-md z-50 flex items-center justify-center">
+          <Image
+            src={images[index]}
+            alt="Anime"
+            width={1000}
+            height={1000}
+            className="rounded-2xl object-contain max-h-[90vh] w-auto"
+          />
+
+          <RxCross1
+            onClick={() => setIsFull(false)}
+            className="
+            absolute
+            top-6
+            right-6
+            text-white
+            text-3xl
+            cursor-pointer
+            "
+          />
         </div>
+      ) : (
+        <>
+          <div className="bg-white/40 backdrop-blur-md p-6 rounded-3xl shadow-xl border border-white/30">
+            <div className="overflow-hidden rounded-2xl ">
+              <ImageBox
+                className=""
+                image={images[index]}
+                setIsFull={setIsFull}
+              />
+            </div>
 
-        <div className="flex items-center justify-between mt-6 px-4">
-          <button
-            onClick={prevImage}
-            className="bg-white p-4 rounded-full shadow-md hover:scale-110 transition-all duration-300 cursor-pointer"
-          >
-            <GrPrevious size={20} />
-          </button>
+            <div className="flex items-center justify-between mt-6 px-4">
+              <button
+                onClick={prevImage}
+                On
+                className="bg-white p-4 rounded-full shadow-md hover:scale-110 transition-all duration-300 cursor-pointer"
+              >
+                <GrPrevious size={20} />
+              </button>
 
-          <p className="text-gray-700 font-semibold text-lg">
-            {index + 1} / {images.length}
-          </p>
+              <div className="flex justify-center gap-2 text-gray-700 text-lg">
+                {images.map((_, i) => (
+                  <button key={i}>
+                    {i === index ? <GoDotFill /> : <GoCircle />}
+                  </button>
+                ))}
+              </div>
 
-          <button
-            onClick={nextImage}
-            className="bg-white p-4 rounded-full shadow-md hover:scale-110 transition-all duration-300 cursor-pointer"
-          >
-            <GrNext size={20} />
-          </button>
-        </div>
-      </div>
+              <button
+                onClick={nextImage}
+                className="bg-white p-4 rounded-full shadow-md hover:scale-110 transition-all duration-300 cursor-pointer"
+              >
+                <GrNext size={20} />
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/ImageBox.js
+++ src/components/ImageBox.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Image from "next/image";
 
-const ImageBox = ({ image }) => {
+const ImageBox = ({ image , setIsFull }) => {
   return (
     <div className="relative w-212.5 h-125 ">
       <Image
+        key={image}
         src={image}
         alt="Anime"
         fill
-        className="object-cover rounded-2xl "
+        className="object-cover rounded-2xl transition-all duration-300 hover:scale-105"
+        onClick={()=>{
+            setIsFull(true)
+        }}
       />
+      
     </div>
   );
 };
